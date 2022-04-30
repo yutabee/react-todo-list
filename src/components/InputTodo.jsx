3c33b06905f874cb048f2d57a79638a6b859e537
yutabee <96982045+yutabee@users.noreply.github.com>
2022-05-01 01:40:33 +0900

    コンポーネント内CSS

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+//コンポーネント内に書くことによってCssを扱いやすくする
+//styleの定義の仕方は通常のCssと違ってstringで書く、プロパティは-使わずcamelcaseで書く
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  borderRadius: "8px",
+  padding: "8px",
+  margin: "8px"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    //styleのあて方も通常と少し変わる
+    <div style={style}>
       <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
